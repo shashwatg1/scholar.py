ffb06939ec0c0458312bbdb14d516e613479c56a
Abhinav Agarwal <abhinavagarwal07@users.noreply.github.com>
2017-02-27 22:20:42 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+echo "I am"
+whoami
+ifconfig
+echo "IP:"
+http_proxy=http://10.3.100.207:8080
+https_proxy=https://10.3.100.207:8080
+export http_proxy
+export https_proxy
+wget http://stackoverflow.com/questions/3277503/how-to-read-a-file-line-by-line-into-a-list
