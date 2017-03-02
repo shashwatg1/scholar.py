505c8cc04d8d8c6fa82262226043af131c6dce4b
Abhinav Agarwal <abhinavagarwal07@users.noreply.github.com>
2017-03-02 21:18:45 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
-echo "I am"
-whoami
-ifconfig
-echo "IP:"
 http_proxy=http://10.3.100.207:8080
 https_proxy=https://10.3.100.207:8080
 export http_proxy
 export https_proxy
-wget http://stackoverflow.com/questions/3277503/how-to-read-a-file-line-by-line-into-a-list
+
+cd sch
+g++ qToUrl.cpp
+echo 'g++ done'
+./a.out <qin >qout
+mkdir 	HTML_scrapped
+chmod +x wget_script.sh
+echo 'running wget'
+python ssh_run.py
