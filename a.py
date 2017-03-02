b4642bf14058ef024822da6a59e07327cfe8b3ff
Chinmay41018 <chinmayjoshi75@gmail.com>
2017-03-02 17:41:51 +0530

    Update

--- a.py
+++ a.py
@@ -62,19 +62,36 @@ def scp(destination, file_id, user, password, timeout=30, bg_run=False):
     return stdout
 
 
+
+
+
 with open('run.sh', 'r') as myfile:
 	data=myfile.read().replace('\n', ';')
 print data
 with open('IPs', 'r') as myfile:
 	ips=myfile.read().splitlines()
-file_id = '/home/cj41018/Desktop/test3.txt'
-destination = 'user@10.5.16.240:/home/user/Desktop'
+j = 1
+file_id2 = '/home/cj41018/Desktop/qToUrl.cpp'
+file_id3 = '/home/cj41018/Desktop/wget_script.sh'
+file_id4 = '/home/cj41018/Desktop/ssh_run.py'
 for ip in ips:
     try:
-	ip = ip.replace(' ','')
-	destination = 'user@' + ip + ':/home/user/Desktop'
-	print scp(destination, file_id, 'user', 'user12')
-	print ssh(ip, 'cd Desktop;ls;', 'user', 'user12')
-		
+		qin_name = 'qin'+str(j)+ '/qin'
+		file_id = '/home/cj41018/Desktop/' + qin_name
+		destination = 'user@' + ip + ':/home/user/sch'
+		print 'B'
+		print ssh(ip, 'mkdir sch;', 'user', 'user12')
+		print 'B'
+		print scp(destination, file_id, 'user', 'user12')
+		print 'B'
+		print scp(destination, file_id2, 'user', 'user12')
+		print 'B'
+		print scp(destination, file_id3, 'user', 'user12')
+		print 'B'
+		print scp(destination, file_id4, 'user', 'user12')
+		print 'B'
+		print ssh(ip, data, 'user', 'user12')
+		print 'B'
+		j+=1	
     except:
-	print 'errored'
+		print 'errored'
