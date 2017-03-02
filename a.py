b287a093672e46df3efc6f832429795f262525a1
Chinmay41018 <chinmayjoshi75@gmail.com>
2017-03-02 15:50:34 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,5 +1,6 @@
 import os
 import tempfile
+import sys
 import pexpect
 def ssh(host, cmd, user, password, timeout=30, bg_run=False):
     """SSH'es to a host using the supplied credentials and executes a command.
@@ -30,25 +31,50 @@ def ssh(host, cmd, user, password, timeout=30, bg_run=False):
 
     return stdout
 
+def scp(destination, file_id, user, password, timeout=30, bg_run=False):
+    """SSH'es to a host using the supplied credentials and executes a command.
+    Throws an exception if the command doesn't return 0.
+    bgrun: run command in the background"""
+
+    fname = tempfile.mktemp()
+    fout = open(fname, 'w')
+
+    options = '-q -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oPubkeyAuthentication=no'
+    if bg_run:
+        options += ' -f'
+    ssh_cmd = 'scp %s %s' % (file_id, destination)
+    child = pexpect.spawn(ssh_cmd, timeout=timeout)
+    child.expect(['password: '])
+    child.sendline(password)
+    child.logfile = fout
+    child.expect(pexpect.EOF)
+    child.close()
+    fout.close()
+
+    fin = open(fname, 'r')
+    stdout = fin.read()
+    fin.close()
+
+    if 0 != child.exitstatus:
+        raise Exception(stdout)
+
+
+    return stdout
+
+
 with open('run.sh', 'r') as myfile:
-        data=myfile.read().replace('\n', ';')
+	data=myfile.read().replace('\n', ';')
 print data
-with open('ip', 'r') as ipf:
-        ip=ipf.read().splitlines()
-        # print ip
-print ip
-for i in ip:
-    print 'ssh server @ '+i
+with open('IPs', 'r') as myfile:
+	ips=myfile.read().splitlines()
+file_id = '/home/cj41018/Desktop/test3.txt'
+destination = 'user@10.5.16.240:/home/user/Desktop'
+for ip in ips:
     try:
-        print ssh(i, data, 'user', 'user12')
+	ip = ip.replace(' ','')
+	destination = 'user@' + ip + ':/home/user/Desktop'
+	print scp(destination, file_id, 'user', 'user12')
+	print ssh(ip, 'cd Desktop;ls;', 'user', 'user12')
+		
     except:
-        print 'errored'
-
-
-# ip_string = '10.5.16.'
-# for ip_end in range(1000):
-#     ip = ip_string + str(ip_end)
-#     try:
-#         print ssh(ip, data, 'user', 'user12')
-#     except:
-#         print 'errored'
+	print 'errored'
