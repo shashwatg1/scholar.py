020ba9f26fe339518535544e633cb2a3d84cb9bc
Abhinav Agarwal <abhinavagarwal07@users.noreply.github.com>
2017-03-04 00:36:45 +0530

    fix

--- curlUrl.sh
+++ curlUrl.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+url=$1
+out_file=$2
 # This code will get the source of the given Url
 # To execute:
 # chmod 755 curlUrl.sh
 # ./curlUrl.sh > out
 
 useragent="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"
-url="https://scholar.google.co.in/scholar?hl=en&q=Andre+Konstantin" 
 #url="http://www.google.co.in/"
 
-curl -A $useragent --cookie cookies.txt --cookie-jar cookie.txt --connect-timeout 10 $url > out
+curl -A $useragent --cookie cookies.txt --cookie-jar cookie.txt --connect-timeout 10 $url > $out_file
